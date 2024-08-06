89c65e108fc446f833ff5901bb6a17faf8c15d55
Manuel Del Pozo <madepozo.dev@gmail.com>
2024-08-05 22:18:37 -0500

    Insert data into the users tables

--- schema.sql
+++ schema.sql
@@ -15,3 +15,12 @@ CREATE TABLE users(
   email VARCHAR(100) NULL,
   PRIMARY KEY (user_id)
 );
+
+INSERT INTO users(name, surname, age, init_date, email)
+VALUES
+  ('Juan', 'Perez', 24, '2015-05-19', 'juanperez@gmail.com'),
+  ('Miguel', 'Lopez', 27, '2017-09-09', 'miguellopez@gmail.com'),
+  ('Diana', 'Torres', 29, '2018-10-11', 'dianatorres@gmail.com'),
+  ('Pedro', 'Aguilar', 31, '2020-03-14', 'pedroaguilar@gmail.com'),
+  ('Veronica', 'Ramos', 35, '2014-07-29', 'veronicaramos@gmail.com'),
+  ('Ismael', 'Diaz', 25, '2016-12-21', 'ismaeldias@gmail.com');
